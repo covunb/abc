6a038314549c3d811aa8a2e462478de2e724bcd4
jace-robin <jace.robin.cov@gmail.com>
2021-04-22 13:06:06 +0000

    add CHSU sub folder

--- CHSU/debugAndFunctions.js
+++ CHSU/debugAndFunctions.js
@@ -1,18 +1,17 @@
-var idNum = 0
-            var pageType = pageType  /*DONT FORGET TO UNCOMMENT THIS AND ADD THE PAGE TYPE*/
-            function addInfo (idNum, name, desc, source, type) {
+var pageType = pageType  /*DONT FORGET TO UNCOMMENT THIS AND ADD THE PAGE TYPE*/
+            function addInfoArray (idNum, name, desc, source, type) {
                 idNum = nameList[idNum] = name; descList[idNum] = desc; sourceList [idNum] = source; pageType [idNum] = type;
             }
-            function getInfo (idNum) {
+            function getInfoArray (idNum) {
                 var gameName = nameList[idNum]; var gameDesc = DescList[idNum]; var gameSource = sourceList[idNum]; var pageType = typeList[idNum];
                 return(gameName, gameDesc, gameSource, pageType);
             }
             
-            function setupPageInfo (idNum, name, desc, source, type) {
+            function loadInfoObject (idNum, name, desc, source, type) {
                 var pageInfo = {idNum: idNum, name: name, desc: desc, source: source, type: type};
            }
-           function getInfo (requestedInfo, idNum) {
-                return pageInfo.requestedInfo[idNum - 1];
+           function getInfoObject (requestedInfo, idNum) {
+                return pageInfo.requestedInfo [idNum - 1];
            }
         
         
